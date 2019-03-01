790f449aa1b1fb8fd48bf6d68518f0784bfc47fd
Student <Student@A125531.ad.csueastbay.edu>
2019-03-01 15:02:13 -0800

    commit 3

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -18,6 +18,7 @@ public class JavaApplication6 {
         // TODO code application logic here
         System.out.println("Hello world");
         System.out.println("goodbye world");
+        int i;
     }
     
 }
